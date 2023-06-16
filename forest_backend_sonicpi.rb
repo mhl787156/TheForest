320aba7a25d7dc31fe861f0781856cfa1944e20c
Mickey Li <mickeyhli@outlook.com>
2023-06-16 23:33:02 +0100

    Found better way of sonicpi backend

--- forest_backend_sonicpi.rb
+++ forest_backend_sonicpi.rb
@@ -1,9 +1,3 @@
-# Welcome to Sonic Pi
-
-samples = "/home/pi/TheForest/Samples/"
-bird_song = "269570__vonora__cuckoo-the-nightingale-duet.wav"
-roads = "469313__2hear__ambient-street-traffic-dublin.wav"
-
 use_bpm 60
 use_osc "localhost", 4559
 
@@ -11,57 +5,36 @@ set :request_stop_samples_list, []
 set :currently_playing, []
 
 define :sample_player_2 do |samples, sname, start, finish|
-  return live_loop sname do
+  # Thread with name seems to work best
+  # Deals with case of multiple starts without stops
+  # Sync deals with case of stopping (only play once)
+  return in_thread do
+    print "Starting thread", sname
     dur = sample_duration samples, sname, start: start, finish: finish
     qdur = quantise(dur, 1) # To make sure the sample finishes on beat.
     
-    # Play full sample 
+    # Play full sample
     s = sample samples, sname,
       start: start, finish: finish,
-      amp: 1, rate: 1, beat_stretch: qdur, release: 0.3, attack: 0.3
+      amp: 1, rate: 1, beat_stretch: qdur, release: 0.5, attack: 0.5
     control s
     
-    # While sample is playing, monitor every beat to see if buttons have been pressed
-    slice_i = 0
-    qdur.times do
-      
-      print slice_i
-      sleep 1
-      slice_i += 1
-      
-      rssl = get[:request_stop_samples_list]
-      print sname, rssl, rssl.include?(sname)
-      if rssl.include? sname
-        print "Requested Stop", sname
-        
-        modrssl = rssl.dup
-        modrssl.delete(sname)
-        set :request_stop_samples_list, modrssl
-        
-        cplay = get[:currently_playing].dup
-        cplay.delete(sname)
-        set :currently_playing, cplay
-        
-        control s, amp: 0, amp_slide: 1
-
-        sample_free samples, sname
-        stop
-      end
-    end
+    print "Syncing on", sname
+    sync sname
+    control s, amp: 0, amp_slide: 1
+    # sample_free samples, sname
+    print "Live loop stopped", sname
+    stop
   end
 end
-
 
 
 live_loop :startcue do #wait for incoming /start OSC message
   use_real_time
   b = sync "/osc*/start"
-  rssl = get[:currently_playing].dup
-  if !rssl.include?(b[0])
-    sample_player_2 samples, b[0], 0, 1.0
-    rssl.push(b[0]) # b is a list so need to get first element
-    set :currently_playing, rssl
-  end
+  print "Received to start", b
+  t = sample_player_2 samples, b[0], 0, 1.0
+  print "Thread", t
 end
 
 live_loop :stopcue do #wait for incvoming /stop OSC message
@@ -69,26 +42,5 @@ live_loop :stopcue do #wait for incvoming /stop OSC message
   print "Watiing on stop message"
   b = sync "/osc*/stop"
   print "Received on stop", b
-  rssl = get[:request_stop_samples_list].dup
-  rssl.push(b[0]) # b is a list so need to get first element
-  set :request_stop_samples_list, rssl
+  cue b[0]
 end
-
-#live_loop :checkstate do
-#  rssl = get[:request_stop_samples_list].dup
-#  print "RSSL:", rssl
-# sleep 4
-#end
-
-
-
-# sb1 = sample_player samples, bird_song, 0, 1.0
-# sr1 = sample_player samples, roads, 0, 0.5
-
-# print sb1
-
-#osc "/stop", "269570__vonora__cuckoo-the-nightingale-duet.wav"
-#reqssl = get[:request_stop_samples_list].dup
-#print(reqssl)
-#reqssl.push("269570__vonora__cuckoo-the-nightingale-duet.wav")
-#set :request_stop_samples_list,reqssl
