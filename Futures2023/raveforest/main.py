bfcdf46954921d6af1417b764685aa44bf13090b
Henry Hickson <henryhickson@btinternet.com>
2023-10-18 14:57:32 +0100

    Testing sequencer value print 4

--- Futures2023/raveforest/main.py
+++ Futures2023/raveforest/main.py
@@ -25,7 +25,7 @@ class Controller():
 
         # HH - adding link into pillar sequencers so we can flash each tube when it's note is playing the sequence
         self.pillar_sequencers = []
-        for p in config["pillars"]:
+        for p in self.num_pillars:
             self.pillar_sequencers.append(self.sound_manager.pillar_data_in_queues[p])
         # HH - end
 
