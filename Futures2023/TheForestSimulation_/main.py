92b9ba7c6b9191777dd51ee3c22cddec283197f8
KhuludAlharthi99 <146655913+KhuludAlharthi99@users.noreply.github.com>
2023-11-01 17:40:13 +0000

    Update main.py

--- Futures2023/TheForestSimulation_/main.py
+++ Futures2023/TheForestSimulation_/main.py
@@ -1,14 +1,16 @@
 from Forest_Swarm import *
 if __name__ == '__main__':
-    S1 = Forest_Swarm(3, 7, 2)
-    S1.agents.append(Forest_Agent_Red(7, 2,[1,0],5, 0.5)) # change rules bias, [1,0] means agent will always run the first rule.
-    S1.agents.append(Forest_Agent_Blue_Green(7, 2,[1,0], 5, 0.5))
-    S1.agents.append(Forest_Agent_Yellow(7, 2,[1,0], 5, 0.5))
-    #S1.agents.append(Forest_Agent_Random(7, 2, 5, 0.5)
+    S1 = Forest_Swarm(6, 6, 2)
+    S1.agents.append(Forest_Agent_Red(6, 2, [0.3, 0.7], 5, 0.5))
+    S1.agents.append(Forest_Agent_Blue_Green(6, 2, [0.3, 0.7], 5, 0.5))
+    S1.agents.append(Forest_Agent_Yellow(6, 2, [0.3, 0.7], 5, 0.5))
+    S1.agents.append(Forest_Agent_Red(6, 2, [0.3, 0.7], 5, 0.5))
+    S1.agents.append(Forest_Agent_Blue_Green(6, 2, [0.3, 0.7], 5, 0.5))
+    S1.agents.append(Forest_Agent_Yellow(6, 2, [0.3, 0.7], 5, 0.5))
+    # S1.agents.append(Forest_Agent_Random(6, 2, 5, 0.5))
     S1.simulate(50)
-    S1.generate_coords_tubes(3)
     S1.save_data()
     S1.animation()
-    
+
 
 
