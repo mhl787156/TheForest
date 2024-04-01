3c8bc304a9ce0fb9093431ac4724c78ce2fd9c42
Suet <suet.lee@bristol.ac.uk>
2024-04-01 17:09:02 +0100

    Deleting the nested folder

--- EvoMusArt2024/EvoMusArt2024/python-sonic/psonic/samples/vinyl.py
+++
@@ -1,8 +0,0 @@
-"""Vinyl"""
-from . import Sample
-
-
-VINYL_BACKSPIN = Sample('vinyl_backspin', 1.06124716553288)
-VINYL_HISS = Sample('vinyl_hiss', 8.0)
-VINYL_REWIND = Sample('vinyl_rewind', 2.6804761904761905)
-VINYL_SCRATCH = Sample('vinyl_scratch', 0.27383219954648524)
--- EvoMusArt2024/EvoMusArt2024/python-sonic/psonic_example.py
+++
@@ -1,6 +0,0 @@
-from psonic import *
-
-set_server_parameter_from_log("127.0.0.1")
-run("play 60")
-
-
